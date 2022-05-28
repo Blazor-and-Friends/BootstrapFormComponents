384dbe4ca01b54ae3b909f377c83b0e77ee38f4c
mike tranchida <Mtranchi@hotmail.com>
2022-05-28 06:08:12 -0400

    Text input with required, maxlength, and min attributes

---
+++ BootstrapFormComponents/Helpers/BfBaseInput.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootstrapFormComponents.Helpers;
+
+public abstract class BfBaseInput<T> : InputBase<T>
+{
+    [Parameter] public bool Required { get; set; }
+}
---
+++ BootstrapFormComponents/Helpers/MinMaxBaseInput.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BootstrapFormComponents.Helpers;
+
+public abstract class MinMaxBaseInput : BfBaseInput<string>
+{
+    [Parameter] public int MaxLength { get; set; } = int.MaxValue;
+
+    [Parameter] public int MinLength { get; set; }
+}
