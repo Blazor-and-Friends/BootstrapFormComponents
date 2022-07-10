8fecc8bd3b40cbe89e9c4e91074bc201ee0f0e86
mike tranchida <Mtranchi@hotmail.com>
2022-07-10 07:15:04 -0400

    Up to ValidationSummary solution

--- BootstrapFormComponents/Helpers/BfStringHelpers.cs
+++ BootstrapFormComponents/Helpers/BfStringHelpers.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace BootstrapFormComponents.Helpers;
 public static class BfStringHelpers
@@ -20,6 +21,41 @@ public static class BfStringHelpers
         }
 
         return sb_.ToString();
+    }
+
+    public static string StripPhoneDigits(this string s)
+    {
+        return Regex.Replace(s, "[^0-9]", "");
+    }
+
+    public static string FormatPhoneNumber(this string s)
+    {
+        if (string.IsNullOrWhiteSpace(s)) return s;
+        s = s.StripPhoneDigits();
+
+        int length = s.Length;
+
+        string countryCode;
+        string areaCode;
+        string exchange;
+        string line;
+
+        switch (length)
+        {
+            case int n when (n <= 3):
+                return $"({s}) -";
+            case int n when (n <= 6):
+                areaCode = s.Substring(0, 3);
+                exchange = s.Substring(3);
+                return $"({areaCode}) {exchange}-";
+            case int n when (n <= 10):
+                areaCode = s.Substring(0, 3);
+                exchange = s.Substring(3, 3);
+                line = s.Substring(6);
+                return $"({areaCode}) {exchange}-{line}";
+            default:
+                return s;
+        }
     }
 
     static BfStringHelpers()
